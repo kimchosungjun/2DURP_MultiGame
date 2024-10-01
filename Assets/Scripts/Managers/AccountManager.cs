17420088ad56ae2abff977df6d09907580dd679f
Sung Jun Cho <chosung0531@naver.com>
2024-10-01 16:47:47 +0900

    Account Manager 생성

--- Assets/Scripts/Managers/AccountManager.cs
+++ Assets/Scripts/Managers/AccountManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.Networking;
 
 public class AccountManager : MonoBehaviour
 {
-    const string AccountURL = "https://docs.google.com/spreadsheets/d/1cAeqkRDgntwtmbEiXPO1pTApx3EPTMZa9mZyRRLA5cw/export?formats=tsv";
+    const string AccountURL = "https://script.google.com/macros/s/AKfycbxCZ06V110BfGdXhyvZYBdKkAPFGUsS3PccPFfDcM-nWJ0ufnctpy4_Rvks_TEADQUv/exec";
 
     IEnumerator Start()
     {
@@ -16,5 +16,35 @@ public class AccountManager : MonoBehaviour
         string _data = www.downloadHandler.text;
 
         print(_data);
+    }
+
+    public bool IsOverlapID()
+    {
+        return false;
+    }
+
+    public void RegistAccount(string id, string password)
+    {
+        WWWForm _form = new WWWForm();
+        _form.AddField("order", "register");
+        _form.AddField("id", id);
+        _form.AddField("password", password);
+        StartCoroutine(DispatchPost(_form));
+    }
+
+    IEnumerator DispatchPost(WWWForm form)
+    {
+        using(UnityWebRequest _www = UnityWebRequest.Post(AccountURL, form))
+        {
+            yield return _www.SendWebRequest();
+            if (_www.isDone)
+            {
+                Debug.Log("완료");
+            }
+            else
+            {
+                Debug.Log("응답이 없습니다.");
+            }
+        }
     }
 }
